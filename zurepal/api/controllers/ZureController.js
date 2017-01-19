a14141b77ddbc11ead268998a6ac0ef9ced4ccd8
nigamg <gaurav@workboard.com>
2017-01-18 23:57:03 -0800

    zure: zure api call

---
+++ zurepal/api/controllers/ZureController.js
@@ -0,0 +1,11 @@
+/**
+ * ZureController
+ *
+ * @description :: Server-side logic for managing zures
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
